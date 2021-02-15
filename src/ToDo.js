f2ae27728744ad9569a182b7cf238357600cb86e
Keeven Oliveira <66172398+ProgKeeven@users.noreply.github.com>
2021-02-15 03:19:33 -0300

    up three

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,13 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './ToDo.css';
 import List from './components/List';
 import ToDoForm from './components/ToDoForm';
 import Item from './components/Item';
 
+const SAVED_ITEMS = "savedItems"
+
 function ToDo() {
 
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS))
+    if(savedItems){
+      setItems(savedItems);
+    }
+
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(SAVED_ITEMS, JSON.stringify(items));
+  }, [items])
+  
   function onAddItem(text){
 
     let item = new Item(text);
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import "../ToDo.css";
 import ImgCheckON from "../assets/check-on.svg";
